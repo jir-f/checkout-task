e2343ac662e00e2896c6cc5e7d5a209a627a8e13
Jiro <jiro.farah@gmail.com>
2020-10-15 01:26:00 +0400

    Reconciliation page and calendar portion animation

--- src/pages/Reconciliation.js
+++ src/pages/Reconciliation.js
@@ -4,13 +4,27 @@ import Prismic from 'prismic-javascript';
 import { RichText } from 'prismic-reactjs';
 import styled from "styled-components";
 import { Image } from '../components/Reconciliation';
+import { opacityAnimation, 
+  verticalAnimation,
+  horizontalAnimation
+} from '../styles/keyframes';
+
+const speed = 0.5;
 
 const Section = styled.section`
-  max-width: 1222px;
+  max-width: 1350px;
   display: flex;
   flex-direction: row;
   margin: auto;
-  padding: 6rem 2rem;
+  padding: 10rem 2rem;
+
+  @media (max-width: 1200px) {
+    flex-wrap: wrap;
+  }
+
+  @media (max-width: 769px) {
+    padding: 6rem 2rem;
+  }
 `;
 
 const Container = styled.div`
@@ -19,12 +33,25 @@ const Container = styled.div`
   align-items: flex-start;
   width: 46%;
   padding-right: 4.25rem;
+  
+  @media (max-width: 1200px) {
+    padding: 2rem 1rem;
+    width: 75%;
+  }
+
+  @media (max-width: 769px) {
+    width: 100%;
+    padding: 0;
+  }
 `;
 
 const H2 = styled.h2`
   font-size: 2rem;
   margin: 1.5rem 0;
   font-weight: 500;
+  overflow: hidden;
+  opacity: 0;
+  animation: ${verticalAnimation(-10)} ${speed}s  0.1s linear 1 forwards;
 `;
 
 const P = styled.p`
@@ -33,6 +60,8 @@ const P = styled.p`
   font-weight: 400;
   font-style: normal;
   line-height: 2rem;
+  opacity: 0;
+  animation: ${verticalAnimation(10)} ${speed}s  0.3s linear 1 forwards;
 `;
 
 const List = styled.ul`
@@ -42,10 +71,16 @@ const List = styled.ul`
 `;
 
 const ListItem = styled.li`
-  font-weight: 400;
   display: flex;
   align-items: center;
   padding: 0.75rem 0;
+  
+  span {
+    font-weight: 400;
+    opacity: 0;
+    animation: ${horizontalAnimation(20)} 0.5s ease-in 1 forwards;
+  }
+
   :before {
     content: '';
     display: inline-block;
@@ -54,6 +89,19 @@ const ListItem = styled.li`
     background: ${props => `url(${props.listStyleImage})`} no-repeat;
     background-size: contain;
     padding-right: 1rem;
+    transform-origin: center;
+    opacity: 0;
+    animation: ${opacityAnimation} 0.5s 1s ease-in 1 forwards;
+  }
+
+  &:nth-child(${props => props.currentItem + 1}) {
+    span {
+      animation-delay: ${props => props.currentItem * 0.5 + 0.8 }s;
+    }
+
+    &:before {
+      animation-delay: ${props => props.currentItem * 0.5 + 0.8}s;
+    }
   }
 `
 
@@ -74,11 +122,10 @@ const Reconciliation = () => {
   }, []);
 
   if (doc) {
-    console.log(doc.data);
     const slice = doc.data.body[0];
     const reconciliationList = slice.primary.features_list;
+    const reconciliationListImage = slice.primary.features_list_icon.url;
 
-    console.log(reconciliationList);
     return (
       <Section id="reconciliation">
         {doc && 
@@ -93,8 +140,11 @@ const Reconciliation = () => {
               <List>
                 {reconciliationList.map((featuredItem, featuredIndex) => {
                   return (
-                    <ListItem listStyleImage={slice.primary.features_list_icon.url} key={featuredIndex}>
-                      {featuredItem.text}
+                    <ListItem 
+                      listStyleImage={reconciliationListImage}
+                      currentItem={featuredIndex}
+                      key={featuredIndex}>
+                      <span>{featuredItem.text}</span>
                     </ListItem>
                   );
                 })}
@@ -107,7 +157,7 @@ const Reconciliation = () => {
     );
   }
   else {
-    return (<h1>Loading...</h1>)
+    return (<Section id="reconciliation" />)
   }
 }
 
